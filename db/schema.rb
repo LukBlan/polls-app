6096aa2f1c4cf1a0ee61001bc284dcca18c155ee
lukblan <luk.blanko@gmail.com>
2024-05-01 10:46:46 -0300

    Add response model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_120737) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_133808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_01_120737) do
     t.integer "poll_id"
     t.string "text"
     t.index ["poll_id"], name: "index_questions_on_poll_id"
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "answer_choice_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,20 @@ User.destroy_all
 Poll.destroy_all
 Question.destroy_all
 AnswerChoice.destroy_all
+Response.destroy_all
 
 ActiveRecord::Base.connection.tables.each do |table|
   ActiveRecord::Base.connection.reset_pk_sequence!(table)
 end
 
-ronaldo = User.create(username: "ronaldo1987")
+ronaldo = User.create!(username: "ronaldo1987")
+tevez = User.create!(username: "tevez1987")
 
-game_poll = Poll.create(author_id: ronaldo.id, title: "Game Theme Poll")
+game_poll = Poll.create!(author_id: ronaldo.id, title: "Game Theme Poll")
 
-elder_ring_question = Question.create(poll_id: game_poll.id, text: "What do you think of Elder Ring?")
+elder_ring_question = Question.create!(poll_id: game_poll.id, text: "What do you think of Elder Ring?")
 
 good_game = AnswerChoice.create!(question_id:  elder_ring_question.id, text: "Good Game")
 bad_game = AnswerChoice.create!(question_id:  elder_ring_question.id, text: "Bad Game")
+
+Response.create!(user_id: tevez.id, answer_choice_id: good_game.id)
